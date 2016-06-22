deb2054c5e652925f0f6bed2f3696b4d69981fd7
JoolzMoore <joolz.moore@gmail.com>
2016-06-22 15:07:16 +1200

    board  cells activated

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 function startGame () {
+  addListeners(document.getElementsByClassName('board')[0].children)
+}
+    // ADD EVENT LISTENERS HERE
+function addListeners (elements) {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', showCell)
+  }
+}
+
+// CREATE FUNCTION showCell
+function showCell (evt) {
+  evt.preventDefault()
+  evt.target.classList.toggle('hidden')
 }
